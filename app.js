dff48603bb422af795134442a24284540019deea
Azer Kazimov <108828542+azerkazimov@users.noreply.github.com>
2022-10-29 06:11:27 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+// 1. Aşağıda verilən kod nümunələrinin nəticələri və səbəbləri detallı şəkildə izah edin
+
+a=10;
+if(a){
+    console.log("a is true");
+    a+=20;
+}else{
+    console.log("a is false");
+}
+console.log(a)
+
+/*
+elan olunmuş "a" dəyişənə "10" dəyəri təyin etdiyimiz üçün "a" boolean true alır və onun üçün 
+"if-else condition" birinci vəziyyəti alir consola => // a is true / a=10+20=30 vəziyyətini alır
+*/
+
+// =========================================================================================================
+
+a=10;
+if(!a){
+    console.log("a is true");
+    a+=20;
+}else{
+    console.log("a is false");
+}
+console.log(a);
+
+/*
+Bu misaldaki condition birinci vəziyyəti "!a" dəyəri (boolean false) aldiğı halda onun ilə əməliyyat aparıb onun 
+üstünə 20 gələcək, amma "a"-nin dəyəri false oluğu halda error verecek. 
+Consol => a is false / 10; 
+*/
+
+// =========================================================================================================
+// 2. Aşağıda adları qeyd olunan funksiyaların kodlarını tamamlayın
+
+function getBiggestNumber(a,b){
+    if (a > b) {
+		console.log(a);
+	} else {
+		console.log(b);
+	}
+}
+getBiggestNumber(10, 5);
+
+// OR
+
+let getBiggestNumber = (a, b) => (a > b ? a : b);
+console.log(getBiggestNumber(10, 5));
+
+// =========================================================================================================
+
+function printNumbersFrom1To100(a,b) {
+  for (let i = a; i <= b; i++) {
+    console.log(i)
+  }  
+}
+printNumbersFrom1To100(1, 100);
